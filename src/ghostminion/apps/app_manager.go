76edd7ac94c535cf5207a4d1895cc8778ec2b1e1
Yechiel Babani <yechielb2000@gmail.com>
2025-03-20 16:32:06 +0200

    add app manager

---
+++ src/ghostminion/apps/app_manager.go
@@ -0,0 +1,55 @@
+package apps
+
+import (
+	"fmt"
+	"sync"
+)
+
+type App interface {
+	Start(wg *sync.WaitGroup)
+	Stop() error
+}
+
+type AppManager struct {
+	apps map[string]App
+	mu   sync.Mutex
+}
+
+func NewAppManager() *AppManager {
+	return &AppManager{
+		apps: make(map[string]App),
+	}
+}
+
+func (am *AppManager) AddApp(name string, app App) {
+	am.mu.Lock()
+	defer am.mu.Unlock()
+	am.apps[name] = app
+}
+
+func (am *AppManager) RemoveApp(name string) {
+	am.mu.Lock()
+	defer am.mu.Unlock()
+	delete(am.apps, name)
+}
+
+func (am *AppManager) StartAll(wg *sync.WaitGroup) {
+	am.mu.Lock()
+	defer am.mu.Unlock()
+
+	for name, app := range am.apps {
+		wg.Add(1)
+		go app.Start(wg)
+		fmt.Printf("Started app: %s\n", name)
+	}
+}
+
+func (am *AppManager) StopAll() {
+	am.mu.Lock()
+	defer am.mu.Unlock()
+
+	for name, app := range am.apps {
+		app.Stop()
+		fmt.Printf("Stopped app: %s\n", name)
+	}
+}
