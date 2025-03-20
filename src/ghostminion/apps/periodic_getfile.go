ffb04650692f41e463c36a2c5773397e1279f6ef
Yechiel Babani <yechielb2000@gmail.com>
2025-03-20 20:39:13 +0200

    add periodic get file

--- src/ghostminion/apps/periodic_getfile.go
+++ src/ghostminion/apps/periodic_getfile.go
@@ -1,24 +1,45 @@
 package apps
 
 import (
+	"errors"
 	"fmt"
-	"log"
+	"ghostminion/executor"
 	"sync"
+	"time"
 )
 
 type PeriodicGetFileApp struct {
 	Path     string
-	MaxSize  int // maximum allowed size in bytes.
-	Interval int
+	MaxSize  int  // maximum allowed size in bytes.
+	Interval int  // in seconds
 	CheckMD5 bool // check if file was changed since last run.
 }
 
 func (c *PeriodicGetFileApp) Start(wg *sync.WaitGroup) {
 	defer wg.Done()
-	if c.Path == "" {
-		log.Fatal("Path must be provided.")
+	currentFileMD5 := ""
+	for {
+		fileContent, err := executor.GetFile(c.Path)
+		if err != nil {
+			fmt.Println("error: ", err)
+			continue
+		}
+		if c.CheckMD5 {
+			command := fmt.Sprintf("md5sum %v", c.Path)
+			fileMD5Output, err := executor.RunCommand(command)
+			if err != nil {
+				fmt.Println("error calculating MD5: ", err)
+				continue
+			}
+			fileMD5 := string(fileMD5Output) // Convert output to a string
+			if currentFileMD5 != fileMD5 {
+				fmt.Println("File content changed. MD5: ", fileMD5)
+				currentFileMD5 = fileMD5
+				fmt.Println("file content: ", fileContent) // save this to db
+			}
+		}
+		time.Sleep(time.Duration(c.Interval) * time.Second)
 	}
-	fmt.Println("Starting PeriodicGetFile app.")
 }
 
 func (c *PeriodicGetFileApp) Stop() error {
@@ -27,5 +48,14 @@ func (c *PeriodicGetFileApp) Stop() error {
 }
 
 func (c *PeriodicGetFileApp) Validate() error {
+	if c.Path == "" {
+		return errors.New("path must be provided")
+	}
+	if c.MaxSize <= 0 {
+		return errors.New("max size must be greater than 0")
+	}
+	if c.Interval <= 0 {
+		return errors.New("interval must be greater than 0")
+	}
 	return nil
 }
