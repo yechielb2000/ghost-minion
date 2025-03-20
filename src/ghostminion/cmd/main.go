a09f1940083e3e790c463899992c783f7157921e
Yechiel Babani <yechielb2000@gmail.com>
2025-03-20 16:41:13 +0200

    remove runcommand and getfile from main

--- src/ghostminion/cmd/main.go
+++ src/ghostminion/cmd/main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"ghostminion/executor"
-	"log"
-)
-
 func main() {
-	fileContent, err := executor.GetFile("/mnt/c/Users/yechi/Desktop/test.py")
-	if err != nil {
-		log.Printf("File error: %v", err.Error())
-	} else {
-		fmt.Println(string(fileContent))
-	}
-	commandOutput, err := executor.RunCommand("pwd")
-	if err != nil {
-		log.Printf("Command error: %v\nstatus: %v", string(commandOutput), err.Error())
-	} else {
-		fmt.Println(string(commandOutput))
-	}
+
 }
