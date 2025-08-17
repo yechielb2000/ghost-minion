20b7ef9640f6a29d048d4e5449f50fcd68135c7d
Yechiel Babani <yechielb2000@gmail.com>
2025-08-17 15:01:58 +0300

    add telemetry field: Current timestamp

--- ghostminion/communication/telemetry.go
+++ ghostminion/communication/telemetry.go
@@ -1,24 +1,29 @@
 package communication
 
-import "ghostminion/config"
+import (
+	"ghostminion/config"
+	"time"
+)
 
 type Telemetry struct {
-	AgentID   string `yaml:"AgentID"`
-	FirstTime bool   `yaml:"FirstTime"`
-	AgentType string `yaml:"AgentName"`
-	IsAlive   bool   `yaml:"isAlive"`
+	AgentID          string `yaml:"AgentID"`
+	FirstTime        bool   `yaml:"FirstTime"`
+	AgentType        string `yaml:"AgentName"`
+	IsAlive          bool   `yaml:"isAlive"`
+	CurrentTimestamp int64  `yaml:"CurrentTimestamp"`
 }
 
-func NewTelemetry(firstTime, isAlive bool) (Telemetry, error) {
+func NewTelemetry(firstTime bool, isAlive bool) (Telemetry, error) {
 	c, err := config.GetConfig()
 	if err != nil {
 		return Telemetry{}, err
 	}
 
 	return Telemetry{
-		AgentID:   c.AgentID,
-		FirstTime: firstTime,
-		AgentType: "GhostMinion",
-		IsAlive:   isAlive,
+		AgentID:          c.AgentID,
+		FirstTime:        firstTime,
+		AgentType:        "GhostMinion",
+		IsAlive:          isAlive,
+		CurrentTimestamp: time.Now().Unix(),
 	}, nil
 }
