7e47cbcd17e64fe9febb438791945112e7a56fe3
Yechiel Babani <51932344+yechielb2000@users.noreply.github.com>
2025-09-08 11:29:34 +0000

    add files

---
+++ ghostminion/cmd/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"ghostminion/apps"
+	"ghostminion/config"
+	"ghostminion/core"
+	"ghostminion/db"
+	"ghostminion/logger"
+	"ghostminion/persistence"
+)
+
+func main() {
+
+	var cfg = config.GetInstance()
+	db.GetInstance()
+	var lgr = logger.GetInstance()
+
+	defer config.DeleteConfig()
+	defer lgr.Close()
+
+	// if err := hider.Hide(); err != nil {
+	// 	return
+	// }
+	lgr.Info("hello world")
+
+	targetId := persistence.GeTargetID()
+	err := cfg.Update(func(c *config.Config) {
+		c.AgentID = targetId
+	})
+	if err != nil {
+		lgr.Error("Error updating agent ID: " + err.Error())
+	}
+
+	lgr.Debug("targetId:", targetId)
+
+	appCore := core.GetInstance()
+	RegisterDefaultApps(appCore, cfg)
+	appCore.Start() // todo: should run security here
+}
+
+func RegisterDefaultApps(core *core.Core, cfg *config.Config) {
+	am := core.AppsManager()
+	for _, appConfig := range cfg.Apps {
+		app, err := apps.NewAppFactory(apps.AppData{
+			Id:     appConfig.Id,
+			Name:   appConfig.Name,
+			Type:   apps.AppType(appConfig.Type),
+			Params: appConfig.Params,
+		})
+		if err != nil {
+			return
+		}
+		am.Register(app)
+	}
+}
